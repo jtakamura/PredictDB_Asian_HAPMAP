f61fcd28021d1766bba5ce4ac1d68d363cb5ad56
jtakamura <jtakamura@luc.edu>
2017-11-08 22:58:45 -0600

    Add files via upload

---
+++ Run_run_files.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+#PBS -N Run_run_files
+#PBS -S /bin/bash
+#PBS -l walltime=24:00:00
+#PBS -l nodes=1:ppn=2
+#PBS -l mem=8gb
+#PBS -o logs/${PBS_JOBNAME}.o${PBS_JOBID}.log
+#PBS -e logs/${PBS_JOBNAME}.e${PBS_JOBID}.err
+cd /home/jennifer/Files_for_PredictDB
+
+bash  run_files.sh
+
+echo "Hi ${USER}, your script ran!" > testjob_output2.txt
+echo "Congratulations, you are a superstar!" >> testjob_output2.txt
+
---
+++ run_files.sh
@@ -0,0 +1,6 @@
+
+for (( j = 1 ; j <= 22; j++))
+do
+        python make_files_PredictDB.py 'Asian' ${j}
+done
+
