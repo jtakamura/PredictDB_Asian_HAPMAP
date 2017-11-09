92663d91b45f0a7c273142c2a0d175751746198f
jtakamura <jtakamura@luc.edu>
2017-11-08 23:03:12 -0600

    Update and rename run_files.sh to 04_run_files.sh

---
+++ 04_run_files.sh
@@ -0,0 +1,9 @@
+
+for (( j = 1 ; j <= 22; j++))
+do
+        python make_files_PredictDB.py 'Asian' ${j}
+done
+
+#bash command for make_files_PredictDB.py
+#enter in file that you want to call and arg[1] in that file which is the pop name
+
--- run_files.sh
+++
@@ -1,6 +0,0 @@
-
-for (( j = 1 ; j <= 22; j++))
-do
-        python make_files_PredictDB.py 'Asian' ${j}
-done
-
