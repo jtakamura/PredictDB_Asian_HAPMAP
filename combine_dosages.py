53af12c8d638e767da844d5bb9cdbc4a6db28ea5
Angela Andaleon <angie.andaleon@gmail.com>
2019-02-01 11:14:14 -0600

    Update combine_dosages.py

--- combine_dosages.py
+++ combine_dosages.py
@@ -1,4 +1,4 @@
-#combine dosages from input pops and combine all the SNPs that overlap to output combined dosages and combined sample files
+#combine dosages from input pops and combine all the SNPs that overlap to output combined dosages and combined sample files and I also added expression
 #by Angela Andaleon (aandaleon@luc.edu)
 from __future__ import division #using python 2 like a fool
 import pandas as pd
@@ -26,11 +26,19 @@ for chr in chrs:
     dosages_only = pop_1.iloc[:, 5:] #can't calculate w/ string columns
     AF = dosages_only.sum(axis = 1) / len(dosages_only.columns) #recalculate allele frequencies
     pop_1.insert(5, "AF", AF) #needs to be index 5
-    pop_1.to_csv("chr" + str(chr) + "_" + combined + ".dosage.txt", sep = " ", columns = None, header = False, index = False)
+    pop_1.to_csv("chr" + str(chr) + "_" + combined + ".dosage.txt.gz", sep = " ", columns = None, header = False, index = False, compression = "gzip")
     
     #write combined samples file
     samples = list(pop_1.columns[6:])
     samples = pd.DataFrame({0: samples, 1: samples}) #I'm going to assume FID and IID are the same
-    samples.to_csv("samples_" + combined + ".txt", sep = " ", columns = None, header = False, index = False)
+    samples.to_csv("samples_" + combined + ".txt", sep = " ", columns = None, header = False, index = False) #zip b/c the files are thicc
     print("Completed with chr. " + str(chr) + ".")    
-print("Completed with combining dosages. Have a nice day :).")
+print("Completed with combining dosages. Now combining expression data.")
+
+#samples = list(pd.read_csv("samples_" + combined + ".txt", header = None, delim_whitespace = True)[0])
+exp_1 = pd.read_csv("/home/jennifer/hapmap_expression_ensid/" + pops[0] + "_expression_ens.txt", delim_whitespace = True, index_col = 0)
+for pop_num in range(1, len(pops)):
+    exp_2 = pd.read_csv("/home/jennifer/hapmap_expression_ensid/" + pops[pop_num] + "_expression_ens.txt", delim_whitespace = True, index_col = 0)
+    exp_1 = exp_1.join(exp_2, how = "inner", lsuffix = "", rsuffix = "") #inner join dosages to only expressions in same gene
+exp = exp_1.loc[:, samples] #reorder and restrict to ppl w/ dosages
+exp.to_csv(combined + "_expression_ens.txt", sep = " ")
